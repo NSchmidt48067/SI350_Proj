d7a8b4cb533311389b7103aa0557347e4715ebd0
CyFi13 <m260102@usna.edu>
2025-11-21 09:11:00 -0500

    i hate github

--- src/index.php
+++ src/index.php
@@ -28,41 +28,50 @@
     <!-- NAV bar that resembles George's graphic design -->
     <nav class="navbar navbar-expand-sm navbar-light bg-light">
       <!-- Left side of nav bar -->
-      <ul class="navbar-nav"> 
-        <!-- more options button: TODO: add something for it... -->
-        <button type="button" class="btn">
-          <i class="bi bi-grid-3x3-gap-fill"></i>
-        </button>
-        <!-- Search button: TODO: add search functionality -->
-        <button type="button" class="btn">
-          <i class="bi bi-search"></i>
-        </button> 
-      </ul>
-      <!-- Center/Title of nav bar -->
-      <a class="navbar-brand mx-auto" href="index.php">Rate My Meal</a>
-      <!-- Right Side of nav bar -->
-      <ul class="navbar-nav">
-        <!-- Dropdown button -->
-        <div class="btn-group">
-          <button type="button" class="btn" id="dropDownMenu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
-            <i class="bi bi-list"></i>
-          </button>
-          <!-- Dropdown Menu: options to add meal and ... -->
-          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropDownMenu">
-            <li><a class="dropdown-item" href="new_meal.php">Suggest Meal</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="aboutUs.html">About Us</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">TODO</a></li>
-          </ul>
-        </div>
-        <!-- profile/login button -->
-        <a href="login.html">
+      <div class="d-flex m-0 p-0">
+        <ul class="navbar-nav"> 
+          <!-- more options button: TODO: add something for it... -->
           <button type="button" class="btn">
-            <i class="bi bi-person-circle"></i>
-          </button> 
-        </a> 
-      </ul>
+            <i class="bi bi-grid-3x3-gap-fill"></i>
+          </button>
+          <!-- Search button: TODO: add search functionality -->
+          <form class="d-flex" role="search">
+              <input class="form-control" id="searchBar" type="search" placeholder="Search" aria-label="Search"/>
+              <button class="btn" type="submit">
+                <i class="bi bi-search"></i>
+              </button>
+            </form>
+        </ul>
+      </div>
+      <!-- Center/Title of nav bar -->
+      <div class="d-flex m-0 p-0 mx-auto">
+        <a class="navbar-brand mx-auto" href="index.php">Rate My Meal</a>
+      </div>
+      <!-- Right Side of nav bar -->
+      <div class="d-flex m-0 p-0">
+        <ul class="navbar-nav">
+          <!-- Dropdown button -->
+          <div class="btn-group">
+            <button type="button" class="btn" id="dropDownMenu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
+              <i class="bi bi-list"></i>
+            </button>
+            <!-- Dropdown Menu: options to add meal and ... -->
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropDownMenu">
+              <li><a class="dropdown-item" href="new_meal.php">Suggest Meal</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="aboutUs.html">About Us</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">TODO</a></li>
+            </ul>
+          </div>
+          <!-- profile/login button -->
+          <a href="login.html">
+            <button type="button" class="btn">
+              <i class="bi bi-person-circle"></i>
+            </button> 
+          </a> 
+        </ul>
+      </div>
     </nav>
 
     <!-- Use AJAX and PHP script to dynamically load meal cards -->
