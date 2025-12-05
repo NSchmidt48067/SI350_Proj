601668af3cdda4f71e023ac972eaf7d362907e8c
NSchmidt48067 <m265646@usna.edu>
2025-12-05 08:10:22 -0500

    Logout button

---
+++ src/logout.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// If a session cookie exists, delete it
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destroy the session
+session_destroy();
+
+// Optional: clear your own app cookies
+// setcookie("your_cookie", "", time() - 3600, "/");
+
+// Redirect to login page
+header("Location: login.html");
+exit();
+?>
